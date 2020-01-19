dc295885cda89c6832abadd3f350bb5d2050cb6f
Zyev Kirill <zyev.99@mail.ru>
2020-01-19 16:05:32 +0700

    adding files

---
+++ laba3/laba3.py
@@ -0,0 +1,15 @@
+from graph import*
+
+brushColor('yellow')
+circle(250, 300, 150)
+brushColor('red')
+circle(300, 250, 20)
+circle(200, 250, 25)
+brushColor('black')
+circle(300, 250, 10)
+circle(200, 250, 10)
+polyline([(235,225), (245, 215)
+   	  (185, 200), (195, 190)])
+#polyline(270,225,350, 255)
+
+run()
